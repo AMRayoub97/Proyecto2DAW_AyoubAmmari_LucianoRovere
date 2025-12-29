59bc7c6220e0accc7f9a079d7211992e9a333258
AMRayoub97 <amrayoub959@gmail.com>
2025-12-29 15:44:59 +0100

    Index:anadir noticias y eventos (solo front-end)

--- bellreguard/resources/views/index.blade.php
+++ bellreguard/resources/views/index.blade.php
@@ -37,7 +37,7 @@
             </div>
 
             <div class="destac">
-                <h1 class="titulo">🔥 DESTACADOS HOY</h1>
+                <h1>🔥 DESTACADOS HOY</h1>
                 <hr>
                 <section id="destacados">
                     <article>
@@ -101,20 +101,96 @@
                 </section>
             </div>
 
-            <h1 class="titulo">ÚLTIMAS NOTICIAS</h1>
-            <div id="noticias">
-                <div style="background-image: url('{{ asset('images/Graffiti_Court_Chronicles.jpeg') }}');">
-                    <h2 class="titulo">Título</h2>
-                    <strong class="titulo">2025-01-12</strong>
+
+            <hr>
+            <div class="notcias-eventos">
+                <div id="noticias">
+                    <h1>ÚLTIMAS NOTICIAS</h1>
+                    <article>
+                        <div class="infoNoticia">
+                            <div class="infoUsuarioNoticia">
+                                <a href="#">
+                                    <img src="{{ asset('images/perfil_default.webp') }}" alt="fotoUsuaroNoticia" target="_blank">
+                                    <strong>Joan Barber Molió</strong>
+                                </a>
+                            </div>
+                            <small>2025-01-12</small>
+                        </div>
+                        <hr>
+                        <a href="#">
+                            <p>El jugador del equipo nacional de baloncesto firmó un contrato histórico que lo convierte en uno de los deportistas mejor pagados del país.</p>
+                        </a>
+                    </article>
+                    <article>
+                        <div class="infoNoticia">
+                            <div class="infoUsuarioNoticia">
+                                <a href="https://www.fcbarcelona.es/es/baloncesto/primer-equipo/staff/4401539/xavi-pascual" target="_blank">
+                                    <img src="{{ asset('images/entrenadores/XaviPascual.avif') }}" alt="fotoUsuaroNoticia">
+                                    <strong>Xavi Pascual</strong>
+                                </a>
+                            </div>
+                            <small>2025-01-12</small>
+                        </div>
+                        <hr>
+                        <a href="#">
+                            <p>El torneo juvenil de baloncesto concluyó este fin de semana, destacando el talento emergente de jóvenes promesas que podrían llegar a la liga profesional.</p>
+                        </a>
+                    </article>
+                    <article>
+                        <div class="infoNoticia">
+                            <div class="infoUsuarioNoticia">
+                                <a href="https://www.realmadrid.com/es-ES/baloncesto/primer-equipo/plantilla/sergio-scariolo" target="_blank">
+                                    <img src="{{ asset('images/entrenadores/Sergio-Scariolo.png') }}" alt="fotoUsuaroNoticia">
+                                    <strong>Sergio Scariolo</strong>
+                                </a>
+                            </div>
+                            <small>2025-01-12</small>
+                        </div>
+                        <hr>
+                        <a href="#">
+                            <p>El estadio principal de la ciudad será sede de un partido internacional de baloncesto, atrayendo a fanáticos y medios de comunicación de todo el mundo.</p>
+                        </a>
+                    </article>
                 </div>
-                <div style="background-image: url('{{ asset('images/Graffiti_Court_Chronicles.jpeg') }}');">
-                    <h2 class="titulo">Título</h2>
-                    <strong class="titulo">2025-01-12</strong>
-                </div>
-                <div style="background-image: url('{{ asset('images/Graffiti_Court_Chronicles.jpeg') }}');">
-                    <h2 class="titulo">Título</h2>
-                    <strong class="titulo">2025-01-12</strong>
-                </div>
+
+                <hr>
+
+                <aside class="eventos">
+                    <h2>ÚLTIMOS EVENTOS</h2>
+                    <div class="calendario">
+                        <iframe src="https://calendar.google.com/calendar/embed?src=84c77a57829987452c49d60bb03eaab97665b000d99116c01f48819584c0168b%40group.calendar.google.com&ctz=Europe%2FMadrid" style="border: 0" frameborder="0" scrolling="no"></iframe>
+                    </div>
+                    <article>
+                        <div class="infoEvento">
+                            <div class="infoUsuarioEvento">
+                                <a href="#"  target="_blank">
+                                    <img src="{{ asset('images/perfil_default.webp') }}" alt="fotoUsuaroNoticia">
+                                    <strong>Ayoub</strong>
+                                </a>
+                            </div>
+                            <small>2025-01-12</small>
+                        </div>
+                        <hr>
+                        <a href="#">
+                            <p>La final del torneo de baloncesto fue emocionante, con ambos equipos mostrando gran habilidad y estrategia, culminando en un marcador ajustado que mantuvo a los aficionados al borde de sus asientos.</p>
+                        </a>
+                    </article>
+                    <article>
+                        <div class="infoEvento">
+                            <div class="infoUsuarioEvento">
+                                <a href="#" target="_blank">
+                                    <img src="{{ asset('images/perfil_default.webp') }}" alt="fotoUsuaroNoticia">
+                                    <strong>Luciano</strong>
+                                </a>
+                            </div>
+                            <small>2025-01-12</small>
+                        </div>
+                        <hr>
+                        <a href="#">
+                         <p>El equipo local logró una impresionante victoria en el campeonato de baloncesto, destacando la actuación de su base estrella con triples decisivos en los últimos minutos.</p>
+                        </a>
+                    </article>
+                </aside>
             </div>
 
         </section>
