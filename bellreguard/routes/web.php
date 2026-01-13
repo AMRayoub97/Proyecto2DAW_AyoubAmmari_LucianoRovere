9e1c0f422e6f76271a1aba1fe68025cdbaf27d35
AMRayoub97 <amrayoub959@gmail.com>
2026-01-13 17:47:10 +0100

    Perfil.edit:Anadir pagina para editar perfil

--- bellreguard/routes/web.php
+++ bellreguard/routes/web.php
@@ -30,7 +30,7 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('equipos', EquipoController::class)->except(['index','show']);
 
     /*-----------------Perfil------------------*/
-    Route::get('perfile/{id}',[UsuarioController::class, 'show'])->name('perfil');
+    Route::resource('perfil', UsuarioController::class);
 });
 
 /*------------------Jugadores-----------------------*/
