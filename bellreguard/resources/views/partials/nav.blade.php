3d23e87b16b04675540c0450aa881e20ff9ed015
Ayoub <amrayoub959@gmail.com>
2026-04-29 11:41:01 +0200

    inicio:mejorar el diseño en version movil

--- bellreguard/resources/views/partials/nav.blade.php
+++ bellreguard/resources/views/partials/nav.blade.php
@@ -1,32 +1,37 @@
 <header>
-    <a href="{{ url('/') }}"><img id="logo" src="{{ asset('images/logo_sin_bg.png') }}" alt="logo del club"></a>
+    <a href="{{ url('/') }}" class="header-logo">
+        <img id="logo" src="{{ asset('images/logo_sin_bg.png') }}" alt="logo">
+        <span>Bellreguard</span>
+    </a>
+
     <nav id="navHeadr">
         <ul>
             <li><a href="{{ url('/') }}">Inicio</a></li>
-            <li><a href="{{route('partidos.index')}}">Partidos</a></li>
+            <li><a href="{{ route('partidos.index') }}">Partidos</a></li>
             <li><a href="{{ route('equipos.index') }}">Equipos</a></li>
             <li><a href="{{ route('jugadores.index') }}">Jugadores</a></li>
         </ul>
     </nav>
+
     <div class="perfilOp">
         @auth
             <strong>Hola, {{ auth()->user()->nombre }}</strong>
         @endauth
 
-            @if(empty(auth()->user()->foto))
-                <img id="fotoPerfil" src="{{asset('/images/perfil_default.webp') }}" alt="Default profile picture">
-            @else
-                <img id="fotoPerfil" src="{{ asset('/images/usuarios/'. auth()->user()->foto) }}" alt="Default profile picture">
-            @endif
+        @if(empty(auth()->user()?->foto))
+            <img id="fotoPerfil" src="{{ asset('/images/perfil_default.webp') }}" alt="foto perfil">
+        @else
+            <img id="fotoPerfil" src="{{ asset('/images/usuarios/' . auth()->user()->foto) }}" alt="foto perfil">
+        @endif
 
         <ul id="perfilLista">
             @guest
-                <li><a href="{{ route('login') }}">Iniciar session</a></li>
-                <li><a href="{{ route('registrar') }}">Registrar</a></li>
+                <li><a href="{{ route('login') }}">Iniciar sesión</a></li>
+                <li><a href="{{ route('registrar') }}">Registrarse</a></li>
             @endguest
             @auth
-                <li><a href="{{ route('perfil.show', auth()->user()->id) }}">Perfil</a></li>
-                <li><a href="{{ route('logout') }}">cerrar session</a></li>
+                <li><a href="{{ route('perfil.show', auth()->user()->id) }}">Mi perfil</a></li>
+                <li><a href="{{ route('logout') }}">Cerrar sesión</a></li>
             @endauth
         </ul>
     </div>
